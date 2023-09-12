42bf1031a328e75df189cd702362997e62634a15
Juan Cruz Perez <70979380+juan-cruz-1@users.noreply.github.com>
2023-09-12 20:48:46 -0300

    initial commit

---
+++ portfolio-basico-main/data.js
@@ -0,0 +1,137 @@
+const proyectos = [
+  {
+    titulo: "Compositor online de música",
+    descripcion:
+      "Página web con compositor online de música. Front y Back completos hechos por mí.",
+    link: "https://musicboxmelodies.com/",
+    fecha: "01/01/2022",
+    imagen: "mbm.png",
+    tecnologias: ["Angular", "Typescript", "SCSS", "NodeJs","GraphQL","ToneJs","Paypal","Social Login"],
+  },
+  {
+    titulo: "Compositor online de música",
+    descripcion:
+      "Página web con compositor online de música. Front y Back completos hechos por mí.",
+    link: "https://musicboxmelodies.com/",
+    fecha: "01/01/2022",
+    imagen: "mbm.png",
+    tecnologias: ["Angular", "Typescript", "SCSS", "NodeJs","GraphQL","ToneJs","Paypal","Social Login"],
+  },
+  {
+    titulo: "Compositor online de música",
+    descripcion:
+      "Página web con compositor online de música. Front y Back completos hechos por mí.",
+    link: "https://musicboxmelodies.com/",
+    fecha: "01/01/2022",
+    imagen: "mbm.png",
+    tecnologias: ["Angular", "Typescript", "SCSS", "NodeJs","GraphQL","ToneJs","Paypal","Social Login"],
+  },
+  {
+    titulo: "Pong",
+    descripcion:
+      "Creación de un juego sencillo estilo Pong, hecho para explicar el uso de la programación orientada a objetos.",
+    link: "https://cacalo.github.io/pong-html/",
+    fecha: "29/03/2023",
+    imagen: "pong.png",
+    tecnologias: ["Javascript"],
+  },
+  {
+    titulo: "Pokédex",
+    descripcion:
+      "Herramienta para ver información de distintos pokémon, inspirada en la pokedex de pokemon diamante brillante. Obtiene los datos de una API.",
+    link: "https://pokedex-puntojson.netlify.app/",
+    fecha: "29/02/2023",
+    imagen: "pokedex.jpg",
+    tecnologias: ["Angular", "Typescript", "SCSS", "PokeApi"],
+  },
+  {
+    titulo: "Test de mecanografía",
+    descripcion:
+      "Juego para ver quién puede escribir más rápido. Se te da un minuto y tenés que escribir la mayor cantidad de letras correctas evitando escribir letras incorrectas.",
+    link: "https://cacalo.github.io/test-mecanografia/",
+    fecha: "29/02/2023",
+    imagen: "mecanografia.png",
+    tecnologias: ["HTML","CSS","Javascript","APIs"],
+  },
+  {
+    titulo: "Contador de truco",
+    descripcion:
+      "Herramienta para ver información de distintos pokémon, inspirada en la pokedex de pokemon diamante brillante. Obtiene los datos de una API.",
+    link: "https://cacalo.github.io/Contador-truco/",
+    fecha: "29/02/2023",
+    imagen: "truco.jpg",
+    tecnologias: ["HTML","CSS","Javascript"],
+  },
+  {
+    titulo: "Herramienta para un 'amigo invisible'",
+    descripcion:
+      "Herramienta que nos permite gestionar varios sorteos de amigos invisibles (una actividad en donde se hacen regalos entre grupos de amigos). Está creada pensada en su eso en teléfonos.",
+    link: "https://amigo-inivisible.netlify.app/",
+    fecha: "08/02/2023",
+    imagen: "amigo-invisible.png",
+    tecnologias: ["Angular", "Ionic", "Typescript"],
+  },
+  
+  {
+    titulo: "Página Shopify",
+    descripcion:
+      "Copia de la landing en front del sitio web de Shopify.dev",
+    link: "https://cacalo.github.io/shopify.dev-clone/",
+    fecha: "05/03/2023",
+    imagen: "shopify.png",
+    tecnologias: ["HTML", "CSS"],
+  },
+  {
+    titulo: "Página Abstract",
+    descripcion:
+      "Copia de la landing en front del sitio web help.abstract.com",
+    link: "https://cacalo.github.io/Abstract---Copia-sitio-web/",
+    fecha: "07/07/2023",
+    imagen: "abstract.png",
+    tecnologias: ["HTML", "CSS"],
+  },
+  {
+    titulo: "Calculadora",
+    descripcion:
+      "Proyecto de práctica de calculadora sencilla.",
+    link: "https://cacalo.github.io/calculadora-basica/",
+    fecha: "08/02/2023",
+    imagen: "calculadora.png",
+    tecnologias: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    titulo: "Ta-Te-Ti",
+    descripcion:
+      "Proyecto sencillo de un juego de Ta-te-ti, en donde vemos lo básico de algoritmos y generamos uno para detectar si algún jugador ganó la partida.",
+    link: "https://cacalo.github.io/Ta-Te-Ti/",
+    fecha: "08/02/2023",
+    imagen: "tateti.jpg",
+    tecnologias: ["HTML", "CSS", "Javascript"],
+  },
+
+];
+
+const informacionPersonal = {
+  nombre: "Perez Juan Cruz",
+  subtitulo: "Desarrollador web",
+  imagen: "img/profile-pic.png",
+  otros: [
+    ["Nacionalidad", "Argentino"],
+    [
+      "Edad",
+      new Date(new Date() - new Date("1998/12/09")).getFullYear()-1970 + " años",
+    ],
+  ],
+  idiomas: [
+    ["Español", "Nativo"],["Inglés", "Muy bueno"],
+  ],
+  tecnologias: [
+    ["Html", 9],["Css", 8],["Javascript", 9],["Typescript", 8],["Angular", 8
+    ],["React",7],["Sql",7],["Node",6],["Python",5]
+  ],
+  redes: [
+    ["github", "https://github.com/juan-cruz-1"],
+    ["linkedin", "https://www.linkedin.com/in/juan-cruz-perez-72a9091b7/"],
+    ["instagram", "https://www.instagram.com/juan_ludomaniatico/"],
+  ]
+};
