ea9d78634e34345e94dc10810b379a2814ccee31
Juan Cruz Perez <70979380+juan-cruz-1@users.noreply.github.com>
2023-09-12 22:16:12 -0300

    Add files via upload

--- portfolio-basico-main/data.js
+++ portfolio-basico-main/data.js
@@ -1,21 +1,21 @@
 const proyectos = [
   {
-    titulo: "Compositor online de música",
+    titulo: "App de reserva de hoteles",
     descripcion:
-      "Página web con compositor online de música. Front y Back completos hechos por mí.",
-    link: "https://musicboxmelodies.com/",
+      "App de reserva de hoteles creada para aplicar fundamentos basicos de React.",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/hotel-reservation-app-main",
     fecha: "01/01/2022",
-    imagen: "mbm.png",
-    tecnologias: ["Angular", "Typescript", "SCSS", "NodeJs","GraphQL","ToneJs","Paypal","Social Login"],
+    imagen: "reservas.jpg",
+    tecnologias: ["React", "Html", "CSS", "NodeJs","Javascript"],
   },
   {
-    titulo: "Compositor online de música",
+    titulo: "Carrito de compras",
     descripcion:
-      "Página web con compositor online de música. Front y Back completos hechos por mí.",
-    link: "https://musicboxmelodies.com/",
+      "Implentacion del back para poder realizar la accion de comprar en esta web de carrito.",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/Productos-y-carrito-front-y-back-main",
     fecha: "01/01/2022",
-    imagen: "mbm.png",
-    tecnologias: ["Angular", "Typescript", "SCSS", "NodeJs","GraphQL","ToneJs","Paypal","Social Login"],
+    imagen: "Carrito.jpg",
+    tecnologias: ["Html", "Javascript", "CSS", "NodeJs","SQL"],
   },
   {
     titulo: "Compositor online de música",
@@ -30,7 +30,7 @@ const proyectos = [
     titulo: "Pong",
     descripcion:
       "Creación de un juego sencillo estilo Pong, hecho para explicar el uso de la programación orientada a objetos.",
-    link: "https://cacalo.github.io/pong-html/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/pong-html-en_vivo",
     fecha: "29/03/2023",
     imagen: "pong.png",
     tecnologias: ["Javascript"],
@@ -39,16 +39,16 @@ const proyectos = [
     titulo: "Pokédex",
     descripcion:
       "Herramienta para ver información de distintos pokémon, inspirada en la pokedex de pokemon diamante brillante. Obtiene los datos de una API.",
-    link: "https://pokedex-puntojson.netlify.app/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/pokedex-main",
     fecha: "29/02/2023",
     imagen: "pokedex.jpg",
-    tecnologias: ["Angular", "Typescript", "SCSS", "PokeApi"],
+    tecnologias: ["Html", "Javascript", "CSS", "PokeApi"],
   },
   {
-    titulo: "Test de mecanografía",
+    titulo: "Test de Mecanografia",
     descripcion:
       "Juego para ver quién puede escribir más rápido. Se te da un minuto y tenés que escribir la mayor cantidad de letras correctas evitando escribir letras incorrectas.",
-    link: "https://cacalo.github.io/test-mecanografia/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/test-mecanografia-main",
     fecha: "29/02/2023",
     imagen: "mecanografia.png",
     tecnologias: ["HTML","CSS","Javascript","APIs"],
@@ -57,7 +57,7 @@ const proyectos = [
     titulo: "Contador de truco",
     descripcion:
       "Herramienta para ver información de distintos pokémon, inspirada en la pokedex de pokemon diamante brillante. Obtiene los datos de una API.",
-    link: "https://cacalo.github.io/Contador-truco/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/Contador-truco-main",
     fecha: "29/02/2023",
     imagen: "truco.jpg",
     tecnologias: ["HTML","CSS","Javascript"],
@@ -76,7 +76,7 @@ const proyectos = [
     titulo: "Página Shopify",
     descripcion:
       "Copia de la landing en front del sitio web de Shopify.dev",
-    link: "https://cacalo.github.io/shopify.dev-clone/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/shopify.dev-clone-Version_en_vivo",
     fecha: "05/03/2023",
     imagen: "shopify.png",
     tecnologias: ["HTML", "CSS"],
@@ -85,7 +85,7 @@ const proyectos = [
     titulo: "Página Abstract",
     descripcion:
       "Copia de la landing en front del sitio web help.abstract.com",
-    link: "https://cacalo.github.io/Abstract---Copia-sitio-web/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/Abstract---Copia-sitio-web-version-en-vivo",
     fecha: "07/07/2023",
     imagen: "abstract.png",
     tecnologias: ["HTML", "CSS"],
@@ -94,7 +94,7 @@ const proyectos = [
     titulo: "Calculadora",
     descripcion:
       "Proyecto de práctica de calculadora sencilla.",
-    link: "https://cacalo.github.io/calculadora-basica/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/calculadora-basica-main",
     fecha: "08/02/2023",
     imagen: "calculadora.png",
     tecnologias: ["HTML", "CSS", "Javascript"],
@@ -103,7 +103,7 @@ const proyectos = [
     titulo: "Ta-Te-Ti",
     descripcion:
       "Proyecto sencillo de un juego de Ta-te-ti, en donde vemos lo básico de algoritmos y generamos uno para detectar si algún jugador ganó la partida.",
-    link: "https://cacalo.github.io/Ta-Te-Ti/",
+    link: "https://github.com/juan-cruz-1/projectos/tree/main/Ta-Te-Ti-main",
     fecha: "08/02/2023",
     imagen: "tateti.jpg",
     tecnologias: ["HTML", "CSS", "Javascript"],
